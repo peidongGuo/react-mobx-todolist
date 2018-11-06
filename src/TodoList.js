e6f4744868d87ef0fab63235c8be046985a25302
guopd <guopd@glodon.com>
2018-11-06 09:56:37 +0800

    finish all function

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,6 +5,7 @@ import "./TodoList.css";
 class TodoList extends Component {
   state = {
     inputItem: "",
+    listFlag: "all",
     allList: [
       { title: "读书", isCompleted: false },
       { title: "看电影", isCompleted: false }
@@ -27,13 +28,18 @@ class TodoList extends Component {
     console.log("输入项：" + this.state.searchKeyword);
   };
   handleComplete = (e, index) => {
-    console.log(e.target);
-    let tmpData = this.state.allList.concat([]);
-    console.log(tmpData);
-    console.log(index);
-    tmpData[index].isCompleted = e.target.value;
+    e.persist();
+    this.setState((prevState, props) => {
+      let tmpData = prevState.allList.concat([]);
+      tmpData[index].isCompleted = e.target.checked;
+      return {
+        allList: tmpData
+      };
+    });
+  };
+  handleChg = listFlag => {
     this.setState({
-      allList: tmpData
+      listFlag: listFlag
     });
   };
   handleDelete = index => {
@@ -74,28 +80,40 @@ class TodoList extends Component {
         />
         <span onClick={this.searchItems}>查询</span>
         <div className="tabs">
-          <span>全部</span>
-          <span>未完成</span>
-          <span>已完成</span>
+          <span onClick={() => this.handleChg("all")}>全部</span>
+          <span onClick={() => this.handleChg("activing")}>未完成</span>
+          <span onClick={() => this.handleChg("completed")}>已完成</span>
         </div>
         <div className="list-all">
-          {this.state.allList.map((item, index) => (
-            <div key={index} className={item.isCompleted ? "completed" : ""}>
-              <input
-                type="checkbox"
-                onChange={e => {
-                  this.handleComplete(e, index);
-                }}
-              />
-              {item.title}
-              <span
-                className="btn-delete"
-                onClick={() => this.handleDelete(index)}
-              >
-                删除
-              </span>
-            </div>
-          ))}
+          {this.state.allList.map((item, index) => {
+            return (
+              (this.state.listFlag === "all" ||
+                (this.state.listFlag === "activing" && !item.isCompleted) ||
+                (this.state.listFlag === "completed" && item.isCompleted)) &&
+              (!this.state.searchKeyword ||
+                item.title.indexOf(this.state.searchKeyword) > -1) && (
+                <div
+                  key={index}
+                  className={item.isCompleted ? "completed" : ""}
+                >
+                  <input
+                    type="checkbox"
+                    defaultChecked={item.isCompleted}
+                    onChange={e => {
+                      this.handleComplete(e, index);
+                    }}
+                  />
+                  {item.title}
+                  <span
+                    className="btn-delete"
+                    onClick={() => this.handleDelete(index)}
+                  >
+                    删除
+                  </span>
+                </div>
+              )
+            );
+          })}
         </div>
 
         <div className="list-activing" />
